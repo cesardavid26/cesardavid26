73026a925fd2c00f483e61742e19639950483d7e
cesardavid26 <cesardavidcp@ufps.edu.co>
2020-03-10 07:25:54 -0500

    primer commit

---
+++ JS/js.js
@@ -0,0 +1,11 @@
+$("#selh3").click(function(event){
+$("#contenedor").load('Colombia.html');
+});
+
+$("#part").click(function(event){
+$("#contenedor").load('partido.html');
+});
+
+$("#contacto").click(function(event){
+$("#contenedor").load('contacto.html');
+});
